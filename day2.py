72aede560680c966303d7b99432b6e4932e5010b
Lisa Lipkin <lipkin@student.chalmers.se>
2019-12-16 10:35:00 +0100

    Solution to day 2 part 1

--- day2.py
+++ day2.py
@@ -1,27 +1,26 @@
 def perform_addition(program, operand_one_position, operand_two_position, result_position):
-	program[result_position] = (int(program[operand_one_position]) + int(program[operand_two_position]))
+	program[result_position] = (program[operand_one_position] + program[operand_two_position])
 
 def perform_multiplication(program, operand_one_position, operand_two_position, result_position):
-	program[result_position] = int(program[operand_one_position]) * int(program[operand_two_position])
+	program[result_position] = program[operand_one_position] * program[operand_two_position]
 
 
 def run_program(program):
 	running = True
 	current_position = 0
-	current_value = int(program[current_position])
+	current_value = program[current_position]
 	while running:
-		print(program)
 		if current_value == 99:
 			print("Program reached the end.")
 			running = False
 		elif current_value == 1:
 			perform_addition(program, program[current_position + 1], program[current_position + 2], program[current_position + 3])
 			current_position += 4
-			current_value = int(program[current_position])
+			current_value = program[current_position]
 		elif current_value == 2:
 			perform_multiplication(program, program[current_position + 1], program[current_position + 2], program[current_position + 3])
 			current_position +=4
-			current_value= int(program[current_position])
+			current_value= program[current_position]
 		else:
 			print("Program found unknown opcode")
 			running = False
@@ -29,7 +28,7 @@ def run_program(program):
 
 file = open('input-files/input_day2_1.txt', 'r')
 input_string = str(file.readline())
-program = input_string.split(',')
+program = list(map(int, input_string.split(',')))
 
 program[1] = 12
 program[2] = 2
